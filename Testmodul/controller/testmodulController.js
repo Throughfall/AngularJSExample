d319c43f04037108c961b87a79949b98a5d71d5e
muel <muel@mkn.de>
2018-03-07 15:14:53 +0100

    Calling the Log-function by the testmodul controller

--- Testmodul/controller/testmodulController.js
+++ Testmodul/controller/testmodulController.js
@@ -1,13 +1,15 @@
 
-var application = angular.module('Testmodul', []);
+var application = angular.module('Testmodul', ['LogService']);
 
-application.controller('TestModulCtrl', function($scope) {
+application.controller('TestModulCtrl', function($scope, log) {
     $scope.testdata = {
         name    :   'Value 1',
         desc    :   '2018-01-01T12:00:00',
         date    :   '2018-01-01T12:00:00',
     };
     
+	// Calling the Log-Function
+    log.debug('CookBookCtrl has been invoked');
     $scope.greetings = 'Willkommen';
     $scope.bye = 'Tschööö';
 
